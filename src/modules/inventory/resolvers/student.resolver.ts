9144ef8cbf21118e9debb995a07d09d6f30378c4
fkaaziebu <frederickaziebu1998@gmail.com>
2026-05-30 16:24:36 +0000

    fix: remove subscription guard on loading categories

--- src/modules/inventory/resolvers/student.resolver.ts
+++ src/modules/inventory/resolvers/student.resolver.ts
@@ -64,7 +64,7 @@ export class StudentResolver {
     });
   }
 
-  @UseGuards(GqlJwtAuthGuard, SubscriptionGuard)
+  @UseGuards(GqlJwtAuthGuard)
   @Query(() => [CategoryTypeClass])
   listOrganizationCategories(
     @Context() context,
